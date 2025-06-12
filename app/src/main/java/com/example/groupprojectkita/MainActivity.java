395ca40007997c0c1aa707a669d29c1684c977db
izkan <izkandarhairi194@gmail.com>
2025-06-12 17:14:48 +0800

    testing commit untuk berkumpulan

--- app/src/main/java/com/example/groupprojectkita/MainActivity.java
+++ app/src/main/java/com/example/groupprojectkita/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-//commit in the building bruh
+//commit in the building bruh i tried bro
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
