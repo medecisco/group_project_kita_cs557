e1bf7b9e51cab83afb40e2e45c193dc7f3181ba5
izkan <izkandarhairi194@gmail.com>
2025-06-12 17:14:09 +0800

    testing commit untuk berkumpulan

--- app/src/main/java/com/example/groupprojectkita/MainActivity.java
+++ app/src/main/java/com/example/groupprojectkita/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+//commit in the building bruh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
